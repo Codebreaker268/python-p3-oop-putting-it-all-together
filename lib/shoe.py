510c382feb5938fedc0bf01a40352048785e0e6e
Godfrey Onyango <godiodhis268@gmail.com>
2024-10-29 18:25:17 +0300

    only

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.set_brand(brand)
+        self.set_size(size)
+
+    def get_brand(self):
+        return self._brand    
+        
+    def set_brand(self,brand):
+        self._brand=brand
+
+    def get_size(self):
+        return self._size 
+
+    def set_size(self,size):
+        if isinstance(size,int):
+            self._size=size
+        else:
+            print("size must be an integer")  
+
+    def cobble(self,):
+        self.condition="New"
+        print("Your shoe is as good as new!")
+        return "The shoe has been repaired."
+           
+        
+
+    brand=property(get_brand,set_brand)
+    size=property(get_size,set_size)    
+
+adida=Shoe("adida",45)  
+    
+  
